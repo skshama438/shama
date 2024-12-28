494368c936f066e8f7448059e36d5a3b4d5783e6
skshama438 <skshama480@gmail.com>
2024-12-28 11:26:34 +0530

    Add files via upload

---
+++ checkig the element is present or not.py
@@ -0,0 +1,14 @@
+a=["yellow","black","red","pink","blue"]
+b="orange"
+c=False
+
+for x in a:
+    if x == b:
+        c = True
+        
+
+if c==True:
+    print("the element is present in the list")
+else:
+    print("the element is not present in the list")
+        
